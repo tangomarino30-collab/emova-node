08f1c4a63702cb966379db0ce30105d32909e2a0
tangomarino30-collab <tangomarino30@gmail.com>
2026-06-26 10:41:49 -0300

    Add API handler for fetching service alerts

---
+++ api/alertas.js
@@ -0,0 +1,7 @@
+export default async function handler(req, res) {
+  const CLIENT_ID = "1455ea4d286148fb8026b4f162c008c3";
+  const CLIENT_SECRET = "0EaEC1da661F4Bf6B3B847272499Dac3";
+  const url = `https://apitransporte.buenosaires.gob.ar/subtes/serviceAlerts?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&json=1`;
+  const data = await fetch(url).then(r => r.json());
+  res.json(data);
+}
